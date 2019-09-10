d8cec73a0b136d0d0d8ab95a8ca46851fd83f3e3
mulefish <finch.wren.swift@gmail.com>
2019-09-09 20:04:27 -0700

    tidy up

--- src/App.js
+++ src/App.js
@@ -13,36 +13,17 @@ class App extends React.Component {
     }
   }
   componentDidMount() { 
-    /*
-    const bgCanvas = document.getElementById("background");
-    const ctx = bgCanvas.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke(); 
+    this.bgCanvas = document.getElementById("background");
+    this.bg = this.bgCanvas.getContext("2d");
+    this.bg.moveTo(0, 0);
+    this.bg.lineTo(200, 100);
+    this.bg.stroke(); 
 
-    const fgCanvas = document.getElementById("foreground");
-    const foreground = fgCanvas.getContext("2d");
-    foreground.beginPath();
-    foreground.arc(95, 50, 40, 0, 2 * Math.PI);
-    foreground.stroke(); 
-*/ 
-// m1 Derek apex -  
-// m2 Laura Merkle - 
-//
-
-this.bgCanvas = document.getElementById("background");
-this.bg = this.bgCanvas.getContext("2d");
-this.bg.moveTo(0, 0);
-this.bg.lineTo(200, 100);
-this.bg.stroke(); 
-
-this.fgCanvas = document.getElementById("foreground");
-this.fg = this.fgCanvas.getContext("2d");
-this.fg.beginPath();
-this.fg.arc(95, 50, 40, 0, 2 * Math.PI);
-this.fg.stroke(); 
-
-
+    this.fgCanvas = document.getElementById("foreground");
+    this.fg = this.fgCanvas.getContext("2d");
+    this.fg.beginPath();
+    this.fg.arc(95, 50, 40, 0, 2 * Math.PI);
+    this.fg.stroke(); 
   }
   fetchDataButton() {
     console.log("hello ")
@@ -51,16 +32,13 @@ this.fg.stroke();
   render() { 
     return (
       <div>
-              <button onClick={this.fetchDataButton} >zoom</button>
-
-<hr></hr>
+      <button onClick={this.fetchDataButton} >zoom</button>
+      <hr></hr>
       <div id="wrapper">
         <canvas id="background" width={this.w} height={this.h} ></canvas> 
         <canvas id="foreground" width={this.w} height={this.w} ></canvas> 
       </div>
       <hr></hr>
-
-
       </div>
     )
   }
