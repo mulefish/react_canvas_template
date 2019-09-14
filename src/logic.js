a8bd62ed0521b30c603da0ddb63af650e36acc8a
mulefish <finch.wren.swift@gmail.com>
2019-09-14 11:51:25 -0700

    TODO: And dragging ability

--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,18 @@
 let mouseIsDown = false;
 
+const getMousePos=(evt)=>{
+    const x = evt.clientX;
+    const y = evt.clientY;
+    const coor = "X coords: " + x + ", Y coords: " + y;
+    return {x:x,y:y};
+}
+
+
 function sayHi() { 
     alert('sayHi')
 }
 function fgClick(evt) {
-    console.log( "mouseIsDOwn ", mouseIsDown , evt )
+    console.log( "mouseIsDOwn ", getMousePos(evt) )
 }
 function fgMouseUp(evt) {
 
